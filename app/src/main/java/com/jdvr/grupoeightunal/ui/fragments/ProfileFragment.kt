f2e97896b532d634d90ee46f382b8d9db997a0e3
Javier Vergara <esp.javiervergara@gmail.com>
2021-12-09 16:57:32 -0500

    implementado el perfil

--- app/src/main/java/com/jdvr/grupoeightunal/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/jdvr/grupoeightunal/ui/fragments/ProfileFragment.kt
@@ -1,21 +1,28 @@
 package com.jdvr.grupoeightunal.ui.fragments
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.jdvr.grupoeightunal.databinding.FragmentProfileBinding
+import java.lang.Exception
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    private val REQUEST_CAMERA_PERMISSION = 1
+    private val REQUEST_IMAGE = 2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +32,37 @@ class ProfileFragment : Fragment() {
 //        return inflater.inflate(R.layout.fragment_profile, container, false)
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkPermission()
+        binding.profileImage.setOnClickListener{
+            if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+                    try {
+                        startActivityForResult(intent, REQUEST_IMAGE)
+                    } catch (e: Exception){
+
+                    }
+                }
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK){
+            if (requestCode == REQUEST_IMAGE){
+                val bitmap = data?.extras?.get("data") as Bitmap
+                binding.profileImage.setImageBitmap(bitmap)
+            }
+        }
+    }
+    private fun checkPermission(){
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+        }
     }
 
 }
